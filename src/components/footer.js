7923f0531d6cbde66b9edfaf4929779bf19950f0
Swapnil Narwade <swapnil.narwade3@gmail.com>
2023-10-26 03:37:03 +0530

    updated footer component

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,7 +7,7 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="text-5xl flex justify-center gap-16 py-2">
+    <div className="text-5xl flex justify-center gap-16 py-2 mb-2">
       <Link href="https://twitter.com/Swxpnil">
         <AiFillTwitterCircle className="text-blue-500" />
       </Link>
